ca76c19696ef14a709e89b98574be88e5c4a67da
Oloyede Oladimeji <oloyedeoladimeji@hotmail.com>
2019-11-10 14:16:05 +0100

    password bcrypt now implemented

--- controllers/usercontroller.js
+++ controllers/usercontroller.js
@@ -1,18 +1,22 @@
+const _ = require('lodash');
 const User = require('../models/user').User;
-var crypto = require('crypto');
+const bcrypt = require('bcrypt');
 
 
 
 
-module.exports.create_user = function(req, res) {
 
-    req.body.password =
+module.exports.create_user =  async function(req, res) {
+
+    const salt = await bcrypt.genSalt(10);
+    req.body.password = await bcrypt.hash(req.body.password, salt);
     User.forge(req.body).save()
         .then((user) => {
+            user = _.pick(user, ['id', 'attributes.name', 'attributes.email']);
            return res.status(200).send(user);
         })
         .catch((error) =>{
-            return res.send(400).send(error);
+            return res.status(400).send(error);
         });
 };
 
