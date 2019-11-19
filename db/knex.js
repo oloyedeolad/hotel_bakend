09304c2860f51eb9479b58cb6c24f4e846eae860
Oloyede Oladimeji <oloyedeoladimeji@hotmail.com>
2019-11-19 16:27:34 +0100

    made a change

--- db/knex.js
+++ db/knex.js
@@ -1,4 +1,4 @@
 var environment = process.env.NODE_ENV || 'development';
-var config = require('../knexfile')[environment];
+var config = require('../knexfile')('production');
 
 module.exports = require('knex')(config);
