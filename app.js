da0cb2b6101101bfc499097914e5aa9187d356e0
Oloyede Oladimeji <oloyedeoladimeji@hotmail.com>
2019-11-23 10:20:55 +0100

    Correcting Port Error

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ const firebaseAuth = require('./FirebaseAuth');
 firebase.initializeApp(config);
 
 const app = express();
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
